d2880000f7b683b10663065570f8c5c0e78c6225
A_Izmaylove <its8my8life@gmail.com>
2022-03-15 15:23:38 +0300

    add Hw algorithm_8_2

---
+++ Algorithm_8_2.py
@@ -0,0 +1,122 @@
+class MyExeption(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+class BinaryTree:
+    def __init__(self, root_obj):
+        self.root = root_obj  # корень
+        self.left_child = None  # левый потомок
+        self.right_child = None  # правый потомок
+
+    # добавить левого потомка
+    def insert_left(self, new_node):
+        try:
+            if new_node >= self.root:
+                raise MyExeption(f'Ошибка! Левый потомок = {new_node} больше корня = {self.root}!')
+        except MyExeption as err:
+            print(err)
+        else:
+            # если у узла нет левого потомка
+            if self.left_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.left_child = BinaryTree(new_node)
+            # если у узла есть левый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+
+    # добавить правого потомка
+    def insert_right(self, new_node):
+        try:
+            if new_node <= self.root:
+                raise MyExeption(f'Ошибка! Правый потомок = {new_node} меньше корня = {self.root}!')
+        except MyExeption as err:
+            print(err)
+        else:
+            # если у узла нет правого потомка
+            if self.right_child is None:
+                # тогда узел просто вставляется в дерево
+                # формируется новое поддерево
+                self.right_child = BinaryTree(new_node)
+            # если у узла есть правый потомок
+            else:
+                # тогда вставляем новый узел
+                tree_obj = BinaryTree(new_node)
+                # и спускаем имеющегося потомка на один уровень ниже
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+
+    # метод доступа к правому потомку
+    @property
+    def get_right_child(self):
+        try:
+            if self.right_child is None:
+                raise MyExeption('Потомка справа не существует!')
+            return self.right_child
+        except MyExeption as err:
+            print(err)
+            return BinaryTree(None)
+
+    # метод доступа к левому потомку
+    @property
+    def get_left_child(self):
+        try:
+            if self.left_child is None:
+                raise MyExeption('Потомка слева не существует!')
+            return self.left_child
+        except MyExeption as err:
+            print(err)
+            return BinaryTree(None)
+
+
+
+    # метод установки корня
+    def set_root_val(self, obj):
+        try:
+            if self.get_left_child.get_root_val and self.get_left_child.get_root_val > obj \
+            or self.get_right_child.get_root_val and self.get_right_child.get_root_val < obj:
+                raise MyExeption('Неверное значение!')
+            self.root = obj
+        except MyExeption as err:
+            print(err)
+
+    # метод доступа к корню
+    @property
+    def get_root_val(self):
+        return self.root
+
+
+
+r = BinaryTree(8)
+
+print(f'Корень - {r.get_root_val}')
+
+print(f'Несуществующий потомок слева - {r.get_left_child.get_root_val}')
+
+print('Неверное значение потомка слева :')
+r.insert_left(12)
+
+print('Вставка потомка слева:')
+r.insert_left(40)
+print(f'Значение потомка слева - {r.get_left_child.get_root_val}')
+print(f'Корень - {r.get_root_val}')
+
+print('Неверное значение потомка справа :')
+r.insert_right(4)
+
+
+print('Вставка потомка справа:')
+r.insert_right(11)
+print(f'Значение потомка справа - {r.get_right_child.get_root_val}')
+print(f'Корень - {r.get_root_val}')
+
+print('Попытка поменять значение корня:')
+r.set_root_val(21)
+print(r.get_root_val)
+
+
