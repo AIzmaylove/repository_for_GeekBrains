41a77e5c15cffe473524a478f63b8377dee0eb27
A_Izmaylove <its8my8life@gmail.com>
2022-03-24 17:31:09 +0300

    add Homework lesson 1

---
+++ lesson_1.js
@@ -0,0 +1,15 @@
+// 1. Вывести соответствующую температуру в градусах по Фаренгейту
+var celsius = 25
+var farengate =  (9 / 5) * celsius + 32
+
+console.log(farengate)
+
+// 2. Объявить две переменные: admin и name
+
+var admin = "Василий"
+var name = admin
+
+console.log(admin)
+
+// 3. в JavaScript будет приоритетнее оператор конкатенации и соответсвенно в JavaScript мы получим строку «1000108»
+
